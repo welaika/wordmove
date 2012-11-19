6b35aecc64c174f8e106e3870ca45679c6f7b046
Ju Liu <ju.liu@welaika.com>
2012-11-19 14:26:26 +0100

    some refactoring

--- spec/remote_host_spec.rb
+++ spec/remote_host_spec.rb
@@ -1,4 +1,8 @@
-require 'spec_helper'
+require 'hashie'
+require 'tempfile'
+require 'wordmove/hosts/local_host'
+require 'wordmove/hosts/remote_host'
+require 'active_support/core_ext'
 
 describe Wordmove::RemoteHost do
 
@@ -7,56 +11,66 @@ describe Wordmove::RemoteHost do
   }
 
   let(:host) {
-    logger = Wordmove::Logger.new
-    logger.level = Logger::INFO
-    config.remote[:logger] = @logger
+    config.remote[:logger] = stub
     host = Wordmove::RemoteHost.new( config.remote )
     host.stub!(:locally_run).and_return( 1 )
     host
   }
 
-  context ".upload_dir" do
+  let(:source_dir) { "foobar" }
+  let(:dest_dir) { "barfoo" }
+  let(:rsync_command) { "rsync" }
+  let(:rsync_flags) { ["-azLKO", "-e"] }
 
+  def rsync_options(ssh_command)
+    [
+      rsync_command,
+      *rsync_flags,
+      ssh_command,
+      anything(),
+      "--delete",
+      ":#{source_dir}/",
+      dest_dir
+    ]
+  end
+
+  context ".upload_dir" do
     it "should use config ports, username and password properly" do
-      host.should_receive(:locally_run).with("rsync", "-azLK", "-e",  "sshpass -p password ssh -p 30000", anything(), "--delete", "username@host:foobar/", "barfoo" )
-      host.upload_dir( "foobar", "barfoo" )
+      host.should_receive(:locally_run).with(*rsync_options("sshpass -p password ssh -p 30000 username@host"))
+      host.upload_dir( source_dir, dest_dir )
     end
 
     it "should skip port if missing" do
       config.remote!.ssh!.port = nil
-      host.should_receive(:locally_run).with("rsync", "-azLK", "-e",  "sshpass -p password ssh", anything(), "--delete", "username@host:foobar/", "barfoo" )
-      host.upload_dir( "foobar", "barfoo" )
+      host.should_receive(:locally_run).with(*rsync_options("sshpass -p password ssh username@host"))
+      host.upload_dir( source_dir, dest_dir )
     end
 
     it "should skip username if missing" do
       config.remote!.ssh!.username = nil
-      host.should_receive(:locally_run).with("rsync", "-azLK", "-e",  "sshpass -p password ssh -p 30000", anything(), "--delete", "host:foobar/", "barfoo" )
-      host.upload_dir( "foobar", "barfoo" )
+      host.should_receive(:locally_run).with(*rsync_options("sshpass -p password ssh -p 30000 host"))
+      host.upload_dir( source_dir, dest_dir )
     end
 
     context "when password is missing" do
-
       before { config.remote!.ssh!.password = nil }
 
       it "should use config ports and username properly" do
-        host.should_receive(:locally_run).with("rsync", "-azLK", "-e",  "ssh -p 30000", anything(), "--delete", "username@host:foobar/", "barfoo" )
-        host.upload_dir( "foobar", "barfoo" )
+        host.should_receive(:locally_run).with(*rsync_options("ssh -p 30000 username@host"))
+        host.upload_dir( source_dir, dest_dir )
       end
 
       it "should skip port if missing" do
         config.remote!.ssh!.port = nil
-        host.should_receive(:locally_run).with("rsync", "-azLK", anything(), "--delete", "username@host:foobar/", "barfoo" )
-        host.upload_dir( "foobar", "barfoo" )
+        host.should_receive(:locally_run).with(*rsync_options("ssh username@host"))
+        host.upload_dir( source_dir, dest_dir )
       end
 
       it "should skip username if missing" do
         config.remote!.ssh!.username = nil
-        host.should_receive(:locally_run).with("rsync", "-azLK", "-e",  "ssh -p 30000", anything(), "--delete", "host:foobar/", "barfoo" )
-        host.upload_dir( "foobar", "barfoo" )
+        host.should_receive(:locally_run).with(*rsync_options("ssh -p 30000 host"))
+        host.upload_dir( source_dir, dest_dir )
       end
-
     end
-
   end
-
 end
