7d181c2d34ce7ca5cd0495e06de5525867e6a34b
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-04-10 16:20:21 +0200

    Remove useless whitespace

--- lib/wordmove/deployer/base.rb
+++ lib/wordmove/deployer/base.rb
@@ -194,7 +194,7 @@ module Wordmove
         command << "--password=#{Shellwords.escape(options[:password])}" if options[:password].present?
         command << "--default-character-set=#{Shellwords.escape(options[:charset])}" if options[:charset].present?
         command << "--database=#{Shellwords.escape(options[:name])}"
-        command << " < #{Shellwords.escape(dump_path)}"
+        command << "< #{Shellwords.escape(dump_path)}"
         puts command.join(" ")
         command.join(" ")
       end
