c7163e49cf06346096b04f810c5c77809765cded
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-07-13 00:35:39 +0200

    Use rspec 3.3

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,4 +21,6 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.example_status_persistence_file_path = "./spec/examples.txt"
 end
