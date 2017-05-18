3892ffa272431dff1c982b57eb730240b82b7dd8
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-05-18 09:37:16 +0200

    fix specs: manually implement removed method silence_stream

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,22 @@ require "wordmove"
 
 Dir[File.expand_path("../support/**/*.rb", __FILE__)].sort.each { |f| require f }
 
+# I don't know from where this method was imported,
+# but since last updates it was lost. I looked about
+# it and discovered in Rails 5 was removed becouse not
+# thread safe. So I'm copying it from the new Rails 5
+# implementation.
+# @see https://github.com/rails/rails/commit/481e49c64f790e46f4aff3ed539ed227d2eb46cb
+def silence_stream(stream)
+  old_stream = stream.dup
+  stream.reopen(RbConfig::CONFIG['host_os'] =~ /mswin|mingw/ ? 'NUL:' : '/dev/null')
+  stream.sync = true
+  yield
+ensure
+  stream.reopen(old_stream)
+  old_stream.close
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
