35ce09d568a4529063e3c4aec22d62a28f69e04d
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-05-29 09:54:30 +0200

    Deployer::Ssh::WpcliSqlAdapter refactored to the base interface

--- lib/wordmove/deployer/ssh/wpcli_sql_adapter.rb
+++ lib/wordmove/deployer/ssh/wpcli_sql_adapter.rb
@@ -1,54 +1,43 @@
 module Wordmove
   module Deployer
     class SshWpcliSqlAdapter < SSH
-      def push_db
-        super
-
-        # Backup remote db
+      def backup_remote_db!
         download_remote_db(local_gzipped_backup_path)
+      end
 
-        # Temporary backup local db
+      def adapt_local_db!
         save_local_db(local_dump_path)
-
-        # search and replace some strings in local db
         run wpcli_search_replace(local_options, remote_options, :vhost)
         run wpcli_search_replace(local_options, remote_options, :wordpress_path)
 
-        # dump adapted database
         local_search_replace_dump_path = local_wp_content_dir.path("search_replace_dump.sql")
         local_gzipped_search_replace_dump_path = local_search_replace_dump_path + '.gz'
-        save_local_db(local_search_replace_dump_path)
 
-        # push updated local db to remote db
+        save_local_db(local_search_replace_dump_path)
         run compress_command(local_search_replace_dump_path)
         import_remote_dump(local_gzipped_search_replace_dump_path)
         local_delete(local_gzipped_search_replace_dump_path)
-
-        # restore original local db
         run mysql_import_command(local_dump_path, local_options[:database])
+      end
+
+      def after_push_cleanup!
         local_delete(local_dump_path)
       end
 
-      def pull_db
-        super
-
-        local_dump_path = local_wp_content_dir.path("dump.sql")
-        local_gzipped_dump_path = local_dump_path + '.gz'
-        local_backup_path = local_wp_content_dir.path("local-backup-#{Time.now.to_i}.sql")
-
-        # Backup and compress local db
+      def backup_local_db!
         save_local_db(local_backup_path)
         run compress_command(local_backup_path)
+      end
 
-        # Download, uncompress and import remote db
+      def adapt_remote_db!
         download_remote_db(local_gzipped_dump_path)
         run uncompress_command(local_gzipped_dump_path)
         run mysql_import_command(local_dump_path, local_options[:database])
-
-        # Adapt local db
         run wpcli_search_replace(remote_options, local_options, :vhost)
         run wpcli_search_replace(remote_options, local_options, :wordpress_path)
+      end
 
+      def after_pull_cleanup!
         local_delete(local_dump_path)
       end
 
