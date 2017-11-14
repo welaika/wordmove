cab4f35abdecb815c473bb5c63d84d77558ca67e
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-11-14 22:28:54 +0100

    fix spec helper method movefile_path_for

--- spec/support/fixture_helpers.rb
+++ spec/support/fixture_helpers.rb
@@ -3,8 +3,12 @@ module FixtureHelpers
     File.join(__dir__, '..', 'fixtures', filename)
   end
 
+  def fixture_root_relative_path_for(filename)
+    File.join('spec', 'fixtures', filename)
+  end
+
   def movefile_path_for(filename)
-    fixture_path_for("movefiles/#{filename}")
+    fixture_root_relative_path_for("movefiles/#{filename}")
   end
 end
 
