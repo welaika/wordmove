44dc96791bdfe6384533963d6bdfae922e5a1cdf
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-12-05 01:10:14 +0100

    mysql doctor

--- lib/wordmove/doctor.rb
+++ lib/wordmove/doctor.rb
@@ -2,11 +2,17 @@ module Wordmove
   class Doctor
     def self.start
       movefile
+      mysql
     end
 
     def self.movefile
       movefile_doctor = Wordmove::Doctor::Movefile.new
       movefile_doctor.validate!
+    end
+
+    def self.mysql
+      mysql_doctor = Wordmove::Doctor::Mysql.new
+      mysql_doctor.check!
     end
   end
 end
---
+++ spec/doctor/mysql_spec.rb
@@ -0,0 +1,31 @@
+describe Wordmove::Doctor::Mysql do
+  let(:movefile_name) { 'multi_environments' }
+  let(:movefile_dir) { "spec/fixtures/movefiles" }
+  let(:doctor) { described_class.new(movefile_name, movefile_dir) }
+
+  context ".new!" do
+    it "implements #check! method" do
+      expect_any_instance_of(described_class).to receive(:check!)
+
+      silence_stream(STDOUT) { doctor.check! }
+    end
+
+    it "calls mysql client check" do
+      expect(doctor).to receive(:mysql_client_doctor)
+
+      silence_stream(STDOUT) { doctor.check! }
+    end
+
+    it "calls mysqldump check" do
+      expect(doctor).to receive(:mysqldump_doctor)
+
+      silence_stream(STDOUT) { doctor.check! }
+    end
+
+    it "calls mysql server check" do
+      expect(doctor).to receive(:mysql_server_doctor)
+
+      silence_stream(STDOUT) { doctor.check! }
+    end
+  end
+end
