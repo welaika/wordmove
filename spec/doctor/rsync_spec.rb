f93deabff5a7ad23adeb10752a93f5c0af979476
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-12-09 22:04:50 +0100

    rsync doctor

---
+++ spec/doctor/rsync_spec.rb
@@ -0,0 +1,11 @@
+describe Wordmove::Doctor::Rsync do
+  let(:doctor) { described_class.new }
+
+  context ".new" do
+    it "implements #check! method" do
+      expect_any_instance_of(described_class).to receive(:check!)
+
+      silence_stream(STDOUT) { doctor.check! }
+    end
+  end
+end
