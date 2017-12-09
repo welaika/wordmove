e5884d5d3bb81e6ffa79adf09ab333b87fce8f72
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-12-09 22:05:21 +0100

    'the doctor is in' banner

--- lib/wordmove/doctor.rb
+++ lib/wordmove/doctor.rb
@@ -1,6 +1,7 @@
 module Wordmove
   class Doctor
     def self.start
+      banner
       movefile
       mysql
       wpcli
@@ -26,5 +27,36 @@ module Wordmove
     def self.ssh; end
 
     def self.lftp; end
+
+    # rubocop:disable Metrics/MethodLength
+    def self.banner
+      paint = <<-'ASCII'
+        .------------------------.
+        |       PSYCHIATRIC      |
+        |         HELP  5¢       |
+        |________________________|
+        ||     .-"""--.         ||
+        ||    /        \.-.     ||
+        ||   |     ._,     \    ||
+        ||   \_/`-'   '-.,_/    ||
+        ||   (_   (' _)') \     ||
+        ||   /|           |\    ||
+        ||  | \     __   / |    ||
+        ||   \_).,_____,/}/     ||
+      __||____;_--'___'/ (      ||
+     |\ ||   (__,\\    \_/------||
+     ||\||______________________||
+     ||||                        |
+     ||||       THE DOCTOR       |
+     \|||         IS [IN]   _____|
+      \||                  (______)
+       `|___________________//||\\
+                           //=||=\\
+                           `  ``  `
+      ASCII
+
+      puts paint
+    end
+    # rubocop:enable Metrics/MethodLength
   end
 end
