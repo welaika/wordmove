1eba347bb9d2572d5c099d0ac4b4c34d93b0cbbb
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2017-12-23 11:36:20 +0100

    fix #436

--- lib/wordmove/doctor/wpcli.rb
+++ lib/wordmove/doctor/wpcli.rb
@@ -10,11 +10,21 @@ module Wordmove
       def check!
         logger.task "Checking local wp-cli installation"
 
-        if system('wp cli check-update --quiet')
+        if in_path? && up_to_date?
           logger.success "wp-cli is correctly installed and up to date"
         else
           logger.error "wp-cli is not installed (or not in your $PATH) or not up to date"
         end
+      end
+
+      private
+
+      def in_path?
+        system('which wp')
+      end
+
+      def up_to_date?
+        `wp cli check-update --format=json`.empty?
       end
     end
   end
