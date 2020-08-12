e627a64beddfa3bddb4449bce3e0172617bfffb8
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2020-08-12 11:42:54 +0200

    `ImportRemoteDump` renamed to `PutAndImportDumpRemotely` and refactored

--- lib/wordmove/actions/ssh/put_and_import_dump_remotely.rb
+++ lib/wordmove/actions/ssh/put_and_import_dump_remotely.rb
@@ -1,39 +1,33 @@
 module Wordmove
   module Actions
     module Ssh
-      class ImportRemoteDump
+      class PutAndImportDumpRemotely
         extend ::LightService::Action
         include Wordmove::Actions::Helpers
         include WordpressDirectory::RemoteHelperMethods
         include WordpressDirectory::LocalHelperMethods
-        expects :options
-        expects :remote_options
-        expects :local_options
-        expects :cli_options
-        expects :logger
-        expects :movefile
-        expects :photocopier
+
+        expects :remote_options,
+                :cli_options,
+                :logger,
+                :photocopier,
+                :db_paths
 
         executed do |context| # rubocop:disable Metrics/BlockLength
-          local_dump_path = local_wp_content_dir.path("dump.sql")
-          local_gzipped_dump_path = local_dump_path + '.gz'
-
-          remote_dump_path = remote_wp_content_dir(
-            remote_options: context.remote_options
-          ).path("dump.sql")
-          remote_gizipped_dump_path = remote_dump_path + '.gz'
-
           Wordmove::Actions::PutFile(
             logger: context.logger,
             photocopier: context.photocopier,
-            command_args: [local_gzipped_dump_path, remote_gizipped_dump_path]
+            command_args: [
+              context.db_paths.local.gzipped_path,
+              context.db_paths.remote.gizipped_path
+            ]
           )
 
           Wordmove::Actions::Ssh::RunRemoteCommand(
             cli_options: context.cli_options,
             logger: context.logger,
             photocopier: context.photocopier,
-            command_args: [uncompress_command(remote_gizipped_dump_path)]
+            command_args: [uncompress_command(file_path: context.db_paths.remote.gizipped_path)]
           )
 
           Wordmove::Actions::Ssh::RunRemoteCommand(
@@ -41,14 +35,14 @@ module Wordmove
             logger: context.logger,
             photocopier: context.photocopier,
             command_args: [mysql_import_command(
-              remote_dump_path, context.remote_options[:database]
+              context.db_paths.remote.path, context.remote_options[:database]
             )]
           )
 
           Wordmove::Actions::DeleteRemoteFile(
             photocopier: context.photocopier,
             logger: context.logger,
-            command_args: [remote_dump_path]
+            command_args: [context.db_paths.remote.path]
           )
         end
       end
