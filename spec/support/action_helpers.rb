cf5f3896a07c182ce04b969a0d476112172a285f
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2020-09-14 00:05:53 +0200

    Document OrganizerContextFactory

--- spec/support/action_helpers.rb
+++ spec/support/action_helpers.rb
@@ -1,6 +1,7 @@
 require 'support/fixture_helpers'
 require 'light-service/testing'
 
+# Test helper class to build the context for Push and Pull organizers.
 class OrganizerContextFactory
   extend ::FixtureHelpers
   include Wordmove::Actions::Ssh::Helpers
@@ -22,6 +23,19 @@ class OrganizerContextFactory
       all: false
     }
 
+  # Build the context for Push or Pull organizer.
+  # @param [String] action
+  # @param [String|Symbol] wordmove_action. Atm only :push or :pull exist
+  # @param [Hash] cli_options
+  #   While we have +DEFAULT_OPTIONS+, any option passed into this Hash
+  #   will overwrite the default one. It is really useful to build context
+  #   based on different fixturized movefiles
+  # @example
+  #   OrganizerContextFactory.make_for(
+  #     described_class,
+  #     :push,
+  #     cli_options: { config: movefile_path_for('multi_environments')}
+  #   )
   def self.make_for(action, wordmove_action, cli_options: {})
     cli_options = DEFAULT_OPTIONS.merge(cli_options)
     movefile = Wordmove::Movefile.new(cli_options, nil, false)
@@ -39,6 +53,10 @@ class OrganizerContextFactory
 end
 
 module ActionHelpers
+  # Calling this method inside an example or inside a `before` block
+  # will silence the logger using `/dev/null` as target device.
+  # Note that you have to call this mocking method before `context` is
+  # initialized, in order to have the mocked logger right into the context.
   def silence_logger!
     allow(Wordmove::Logger).to receive(:new).and_return(Wordmove::Logger.new('/dev/null'))
   end
