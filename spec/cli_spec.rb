1322058e0e3de54e93ea8dff0a261578abeac368
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2020-10-01 00:32:08 +0200

    Fixed cli_spec

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,50 +2,49 @@ require 'spec_helper'
 
 describe Wordmove::CLI do
   let(:cli) { described_class.new }
-  let(:deployer) { double("deployer") }
   let(:options) { {} }
 
-  before do
-    allow(Wordmove::Deployer::Base).to receive(:deployer_for).with(options).and_return(deployer)
-  end
-
-  context "#init" do
-    it "delagates the command to the Movefile Generator" do
+  context '#init' do
+    it 'delagates the command to the Movefile Generator' do
       expect(Wordmove::Generators::Movefile).to receive(:start)
       cli.invoke(:init, [], options)
     end
   end
 
-  context "#doctor" do
-    it "delagates the command to Doctor class" do
+  context '#doctor' do
+    it 'delagates the command to Doctor class' do
       expect(Wordmove::Doctor).to receive(:start)
       cli.invoke(:doctor, [], options)
     end
   end
 
-  context "#pull" do
-    context "without a movefile" do
-      it "it rescues from a MovefileNotFound exception" do
+  context '#pull' do
+    context 'without a movefile' do
+      it 'it rescues from a MovefileNotFound exception' do
         expect { cli.invoke(:pull, []) }.to raise_error SystemExit
       end
     end
   end
 
-  context "#list" do
-    subject { cli.invoke(:list, []) }
+  context '#list' do
+    subject do
+      silence_stream($stdout) do
+        cli.invoke(:list, [])
+      end
+    end
     let(:list_class) { Wordmove::EnvironmentsList }
     # Werdmove::EnvironmentsList.print should be called
-    it "delagates the command to EnvironmentsList class" do
+    it 'delagates the command to EnvironmentsList class' do
       expect(list_class).to receive(:print)
       subject
     end
 
     context 'without a valid movefile' do
-      context "no movefile" do
+      context 'no movefile' do
         it { expect { subject }.to raise_error SystemExit }
       end
 
-      context "syntax error movefile " do
+      context 'syntax error movefile ' do
         before do
           # Ref. https://github.com/ruby/psych/blob/master/lib/psych/syntax_error.rb#L8
           # Arguments for initialization: file, line, col, offset, problem, context
@@ -59,96 +58,25 @@ describe Wordmove::CLI do
       end
     end
 
-    context "with a movefile" do
+    context 'with a movefile' do
       subject { cli.invoke(:list, [], options) }
       let(:options) { { config: movefile_path_for('Movefile') } }
-      it "invoke list without error" do
+      it 'invoke list without error' do
         expect { subject }.not_to raise_error
       end
     end
   end
 
-  context "#push" do
-    context "without a movefile" do
-      it "it rescues from a MovefileNotFound exception" do
+  context '#push' do
+    context 'without a movefile' do
+      it 'it rescues from a MovefileNotFound exception' do
         expect { cli.invoke(:pull, []) }.to raise_error SystemExit
       end
     end
   end
 
-  context "--all" do
+  context '--all' do
     let(:options) { { all: true, config: movefile_path_for('Movefile') } }
     let(:ordered_components) { %i[wordpress uploads themes plugins mu_plugins languages db] }
-
-    context "#pull" do
-      it "invokes commands in the right order" do
-        skip 'Moved this logic into service objects'
-
-        ordered_components.each do |component|
-          expect(deployer).to receive("pull_#{component}")
-        end
-        cli.invoke(:pull, [], options)
-      end
-
-      context "with forbidden task" do
-        let(:options) { { all: true, config: movefile_path_for('with_forbidden_tasks') } }
-
-        it "does not pull the forbidden task" do
-          skip 'Moved this logic into service objects'
-          expected_components = ordered_components - [:db]
-
-          expected_components.each do |component|
-            expect(deployer).to receive("pull_#{component}")
-          end
-          expect(deployer).to_not receive("pull_db")
-
-          silence_stream(STDOUT) { cli.invoke(:pull, [], options) }
-        end
-      end
-    end
-
-    context "#push" do
-      it "invokes commands in the right order" do
-        skip 'Moved this logic into service objects'
-
-        ordered_components.each do |component|
-          expect(deployer).to receive("push_#{component}")
-        end
-        cli.invoke(:push, [], options)
-      end
-
-      context "with forbidden task" do
-        let(:options) { { all: true, config: movefile_path_for('with_forbidden_tasks') } }
-
-        it "does not push the forbidden task" do
-          skip 'Moved this logic into service objects'
-
-          expected_components = ordered_components - [:db]
-
-          expected_components.each do |component|
-            expect(deployer).to receive("push_#{component}")
-          end
-          expect(deployer).to_not receive("push_db")
-
-          silence_stream(STDOUT) { cli.invoke(:push, [], options) }
-        end
-      end
-    end
-
-    context "excluding one of the components" do
-      it "does not invoke the escluded component" do
-        skip 'Moved this logic into service objects'
-
-        excluded_component = ordered_components.pop
-        options[excluded_component] = false
-
-        ordered_components.each do |component|
-          expect(deployer).to receive("push_#{component}")
-        end
-        expect(deployer).to_not receive("push_#{excluded_component}")
-
-        cli.invoke(:push, [], options)
-      end
-    end
   end
 end
