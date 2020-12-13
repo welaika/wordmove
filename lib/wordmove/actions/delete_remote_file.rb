749ad4c85481a7dcbaf374fb0f2899afd3cbaa33
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2020-12-13 17:19:03 +0100

    Document DeleteLocalFile

--- lib/wordmove/actions/delete_remote_file.rb
+++ lib/wordmove/actions/delete_remote_file.rb
@@ -1,5 +1,7 @@
 module Wordmove
   module Actions
+    # Delete a remote file
+    # @note This action is *not* meant to be organized, but as a standalone one.
     class DeleteRemoteFile
       extend LightService::Action
 
@@ -7,6 +9,11 @@ module Wordmove
               :logger,
               :command_args
 
+      # @!method execute
+      #   @param photocopier [Photocopier]
+      #   @param logger [Wordmove::Logger]
+      #   @param command_args ((String) remote file path, (String) local file path)
+      #   @return [LightService::Context] Action's context
       executed do |context|
         command = 'delete'
 
