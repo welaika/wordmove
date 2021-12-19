9717a5d0db2cb86b72328a886c28a0982e2838b7
Alessandro Fazzi <alessandro.fazzi@welaika.com>
2021-12-19 21:54:56 +0100

    Update rspec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,6 +36,7 @@ RSpec.configure do |config|
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
+    mocks.verify_doubled_constant_names = true
   end
 
   config.example_status_persistence_file_path = './spec/examples.txt'
